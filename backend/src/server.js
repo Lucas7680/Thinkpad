0913d5ab847c41169a1c87691d82d081de5c245d
Daniyal Ahmed <da9033431@gmail.com>
2025-05-18 17:51:42 +0500

    updation

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,33 +7,21 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-// const __dirname = path.resolve();
 
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "https://thinkpad-git-main-daniyal-ahmeds-projects-c363bc8e.vercel.app/",
+      "https://thinkpad.vercel.app" // Add final deployed frontend here
     ],
   })
 );
 
 app.use(express.json());
 app.use(rateLimiter);
-
 app.use("/api/notes", notesRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+await connectDb(); // Ensure DB is connected before handling requests
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-
-connectDb().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server Listening at port ${PORT}`);
-  });
-});
+export default app;
