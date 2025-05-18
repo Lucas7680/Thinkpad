36915a67c439fe527972f9fe8f0cb935548a3171
Daniyal Ahmed <da9033431@gmail.com>
2025-05-18 17:34:11 +0500

    server updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,27 +8,35 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? ["https://thinkpad.vercel.app"] // your final deployed frontend URL
+    : [
+        "http://localhost:5173",
+        "https://thinkpad-git-main-daniyal-ahmeds-projects-c363bc8e.vercel.app/",
+      ];
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // if needed for cookies/auth headers
+  })
+);
+
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 connectDb().then(() => {
   app.listen(PORT, () => {
